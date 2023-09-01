bb9bc3141b4f1892349da203045cace2704485c1
eugene-levdik <eugene.levdik@gmail.com>
2023-09-02 00:49:53 +0300

    decrease accuracy

--- multiscale_convergence_1.py
+++ multiscale_convergence_1.py
@@ -6,15 +6,15 @@ from grating import Grating
 if __name__ == '__main__':
     smaller_scale = 6.3
     n_range = [1, 10, 20, 30]
-    for n in n_range:
-        amps = []
-        d_range = range(2, 17)
+    d_range = range(2, 17)
+    for d in d_range:
+    	for n in n_range:
 
-        incidence = Incidence(angle=10)
-        grating = Grating.multiscale_lamellar_random(period=n * smaller_scale, n=n,
+	    incidence = Incidence(angle=10)
+            grating = Grating.multiscale_lamellar_random(period=n * smaller_scale, n=n,
                                                      thickness=0.5, max_permittivity=2.1, seed=1)
-        for d in d_range:
-            x = solve_diffraction(d, d, grating, incidence, accuracy=1e-6)
+            
+            x = solve_diffraction(d, d, grating, incidence, accuracy=1e-5)
             amp = abs(x.full(asvector=True)[2 ** (d - 1)])
-            amps.append(amp)
             print(n, d, amp)
+
