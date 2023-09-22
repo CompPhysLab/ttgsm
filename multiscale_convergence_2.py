ceb668880c5e41b0306e766961e1ce719bc827a6
eugene-levdik <eugene.levdik@gmail.com>
2023-09-22 20:38:40 +0300

    minor

--- multiscale_convergence_2.py
+++ multiscale_convergence_2.py
@@ -5,16 +5,15 @@ from grating import Grating
 
 if __name__ == '__main__':
     smaller_scale = 6.3
-    n_range = [1, 10, 25, 50]
-    for n in n_range:
-        amps = []
-        d_range = range(2, 17)
+    n_range = [100]
+    d_range = range(2, 17)
 
-        incidence = Incidence(angle=10)
-        grating = Grating.multiscale_lamellar_random(period=n * smaller_scale, n=n,
-                                                     thickness=0.5, max_permittivity=2.1, seed=1)
-        for d in d_range:
-            x = solve_diffraction(d, d, grating, incidence, accuracy=1e-6)
+    for d in d_range:
+        for n in n_range:
+
+            incidence = Incidence(angle=10)
+            grating = Grating.multiscale_lamellar_random(period=n * smaller_scale, n=n,
+                                                     thickness=0.25, max_permittivity=2.1, seed=1)
+            x = solve_diffraction(d, d, grating, incidence, accuracy=1e-5)
             amp = abs(x.full(asvector=True)[2 ** (d - 1)])
-            amps.append(amp)
             print(n, d, amp)
