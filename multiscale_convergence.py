b0e20cd202c940a19917dc76d9b6dc8b36e61c24
Евгений Левдик <eugene.levdik@gmail.com>
2023-09-27 15:09:44 +0200

    make one file for convergence tests with input

--- multiscale_convergence.py
+++ multiscale_convergence.py
@@ -1,24 +1,35 @@
 from diffraction_solver import solve_diffraction
 from incidence import Incidence
 from grating import Grating
+import logging
 
 
-if __name__ == '__main__':
+logging.basicConfig(filename='multiscale.log', filemode='w', format='%(message)s')
+
+try:
     smaller_scale = 6.3
 
-    print('Input n')
-    n = int(input())
+    print('n range:', end='\t\t')
+    n_range = list(map(int, input().split(',')))
 
-    print('Input d')
-    d = int(input())
-    
-    print('Input accuracy (number of digits)')
-    accuracy = 10 ** -int(input())
+    print('d range from:', end='\t')
+    d_from = int(input())
+    print('d range to:', end='\t')
+    d_to = int(input())
+    d_range = range(d_from, d_to)
 
-    incidence = Incidence(angle=10)
-    grating = Grating.multiscale_lamellar_random(period=n * smaller_scale, n=n,
-                                                 thickness=0.25, max_permittivity=2.1, seed=1)
+    print('thickness:', end='\t')
+    thickness = float(input())
 
-    x = solve_diffraction(d, d, grating, incidence, accuracy=accuracy)
-    amp = abs(x.full(asvector=True)[2 ** (d - 1)])
-    print(n, d, amp)
+    for d in d_range:
+        for n in n_range:
+
+            incidence = Incidence(angle=10)
+            grating = Grating.multiscale_lamellar_random(period=n * smaller_scale, n=n,
+                                                         thickness=thickness, max_permittivity=2.1, seed=1)
+            x = solve_diffraction(d, d, grating, incidence, accuracy=1e-5)
+            amp = abs(x.full(asvector=True)[2 ** (d - 1)])
+            # print(n, d, amp)
+            logging.warning(f'{n} {d} {amp}')
+except Exception as e:
+    logging.warning(e)
